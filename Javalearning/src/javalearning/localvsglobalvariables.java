26d1a6f7f50708eb7f9d8ed661a147113a93ffa9
Siva Mandali <Siva.Mandali@jbhunt.com>
2018-07-07 21:29:46 -0500

    enter new line

---
+++ Javalearning/src/javalearning/localvsglobalvariables.java
@@ -0,0 +1,27 @@
+package javalearning;
+
+public class localvsglobalvariables {
+	// these are global variables
+	String name="Tom";
+	int age= 25;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		// i is local variable in main method
+		int i=10;
+	System.out.println(i);
+	localvsglobalvariables obj=new localvsglobalvariables();
+	
+	System.out.println(obj.age);
+	
+	}
+	
+	public void sum() {
+		//i,bare local variables in sum method
+		int i=15;
+		int b=20;
+		System.out.println(i+b);
+		
+	}
+
+}
