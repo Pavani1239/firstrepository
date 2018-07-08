74c55944be11d7f67b1f74f10298ee1184bb1a5f
Siva Mandali <Siva.Mandali@jbhunt.com>
2018-07-07 22:16:31 -0500

    added class

---
+++ Testproject_Git/src/Testproject_git/Testclass.java
@@ -0,0 +1,11 @@
+package Testproject_git;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test git project");
+
+	}
+
+}
